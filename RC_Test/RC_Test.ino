8d0d2b389e9715b1de46819f83121c371e716336
Kellen Yu <you@example.com>
2024-07-17 20:24:20 -0400

    changes

--- RC_Test/RC_Test.ino
+++ RC_Test/RC_Test.ino
@@ -1,12 +1,51 @@
-#define DAC_OUT PIN_PA4
+#define DAC_OUT PIN_PA6
+#define DISCHARGE PIN_PA5
+#define CHARGE PIN_PA7
+#define ADC_IN PIN_PB1
+
+#define R 100.0;
 
 void setup() {
-  DACReference(INTERNAL1V1);
+  Serial.begin(57600);
+  DACReference(INTERNAL1V5);
+  analogReference(INTERNAL2V5);
+  
+  //init pin modes
   pinMode(DAC_OUT, OUTPUT);
+  pinMode(CHARGE, OUTPUT);
+  pinMode(DISCHARGE, OUTPUT);
+  pinMode(ADC_IN, INPUT);
+  
+  //init pin states
+  digitalWrite(CHARGE, LOW);
+  digitalWrite(DISCHARGE, HIGH);
+  analogWrite(DAC_OUT, 0);
 }
 
 void loop() {
+  //Discharge Capaciator and prep DAC
+  digitalWrite(DISCHARGE, HIGH);
+  digitalWrite(CHARGE, LOW);
   analogWrite(DAC_OUT, 255);
-  // put your main code here, to run repeatedly:
+  delay(50);
+
+  //Charge Capciator
+  long initial_us = micros();
+  digitalWrite(DISCHARGE, LOW);
+  digitalWrite(CHARGE, HIGH);
+
+  //Wait until v(t) = 0.63v_DAC
+  while(analogRead(ADC_IN) < 387) {
+    //do nothing
+  }
+
+  long delta_us = micros() - initial_us;
+  double delta_t = delta_us * pow(10, -6);
+  double c = delta_t/R;
+  Serial.print("c: ");
+  Serial.print(c);
+  Serial.print(", t:");
+  Serial.println(delta_t);
+
 
 }
