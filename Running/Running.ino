fa77e5fd961e326deed0351b6da9229a9695fd1e
Kellen Yu <you@example.com>
2024-07-27 17:29:03 -0400

    working on interrupts

--- Running/Running.ino
+++ Running/Running.ino
@@ -1,5 +1,6 @@
 #include <avr/sleep.h>
 
+//Pinout
 #define DAC_OUT PIN_PA6
 #define DISCHARGE PIN_PA5
 #define CHARGE PIN_PA7
@@ -10,75 +11,114 @@
 #define GRN_LED PIN_PB0
 
 #define AVERAGE_RES 10
-double times[AVERAGE_RES] = {};
-int time_i = 0;
+
+//Coefficients for linear model
+#define M 117.1875014
+#define B -101.9550907
+
+//user defined moisture ranges (acceptable range)
+#define RL 0.6
+#define RH 0.7
 
 void setup() {
   set_sleep_mode(SLEEP_MODE_PWR_DOWN);
-  
+  sleep_enable();
+
   DACReference(INTERNAL1V5);
   analogReference(INTERNAL2V5);
-  
-  //init pin modes
+
+  //init used pin modes
   pinMode(ADC_IN, INPUT);
   pinMode(PTN_IN, INPUT);
-  
+
   pinMode(RED_LED, OUTPUT);
   pinMode(GRN_LED, OUTPUT);
   pinMode(DAC_OUT, OUTPUT);
   pinMode(CHARGE, OUTPUT);
   pinMode(DISCHARGE, OUTPUT);
-  
+
+  //init other pins
+  pinMode(PIN_PB3, OUTPUT);
+  pinMode(PIN_PB2, OUTPUT);
+  pinMode(PIN_PA2, OUTPUT);
+  pinMode(PIN_PA1, OUTPUT);
+  pinMode(PIN_PA0, OUTPUT);
+
   //init pin states
   digitalWrite(CHARGE, LOW);
   digitalWrite(DISCHARGE, LOW);
+  digitalWrite(GRN_LED, LOW);
+  digitalWrite(RED_LED, LOW);
   analogWrite(DAC_OUT, 0);
+
 }
 
 void loop() {
-  
+  double moistureLevel = getMoistureLevel(getRiseTime());
+  if (moistureLevel <= 0.6) {
+    digitalWrite(RED_LED, HIGH);
+  }
+  else if (moistureLevel >= 0.6) {
+    digitalWrite(GRN_LED, HIGH);
+    digitalWrite(RED_LED, HIGH);
+  }
+  else {
+    digitalWrite(GRN_LED, HIGH);
+  }
+  delay(5000);
+  sleep();
 }
 
 void sleep() {
+  digitalWrite(DISCHARGE, LOW);
+  digitalWrite(CHARGE, LOW);
+  digitalWrite(GRN_LED, LOW);
+  digitalWrite(RED_LED, LOW);
+  analogWrite(DAC_OUT, 0);
+  //Disable ADC
   ADC0.CTRLA &= ~ADC_ENABLE_bm;
+  PORTA.
+  sleep_cpu();
+}
+
+ISR() {
+  //enable ADC
+  ADC0.CTRLA |= ADC_ENABLE_bm;
   
 }
 
+double getMoistureLevel(double x) {
+  return M * x + B;
+}
+
 double getRiseTime() {
-  digitalWrite(DISCHARGE, HIGH);
-  digitalWrite(CHARGE, LOW);
-  analogWrite(DAC_OUT, 255);
+  double tSum = 0;
+  for (int i = 0; i < AVERAGE_RES; i++) {
+    digitalWrite(DISCHARGE, HIGH);
+    digitalWrite(CHARGE, LOW);
+    analogWrite(DAC_OUT, 255);
 
-  //Discharge cap
-  while (readADC() > 15) {
+    //Discharge cap
+    while (readADC() > 15) {
+    }
+
+    long t0 = micros();
+    digitalWrite(DISCHARGE, LOW);
+    digitalWrite(CHARGE, HIGH);
+
+    while (readADC() < 637) {
+    }
+
+    long delta_t = micros - t0;
+    tSum += delta_t;
   }
-
-  long t0 = micros();
-  digitalWrite(DISCHARGE, LOW);
-  digitalWrite(CHARGE, HIGH);
-
-  while (readADC() < 637) {
-  }
-
-  long delta_t = micros - t0;
-  times[time_i] = delta_t;
-  time_i++;
-  time_i %= AVERAGE_RES;
-  return getAvgTime();
+  return (tSum / AVERAGE_RES) * pow(10, -6);
 }
 
 double readADC() {
   double sum = 0;
-  for (int i=0; i<5; i++) {
+  for (int i = 0; i < 5; i++) {
     sum += analogRead(ADC_IN);
   }
-  return sum/5;
-}
-
-double getAvgTime() {
-  double sum = 0;
-  for (int i=0; i<AVERAGE_RES; i++)  {
-    sum += times[i]
-  }
-  return sum/AVERAGE_RES;
+  return sum / 5;
 }
